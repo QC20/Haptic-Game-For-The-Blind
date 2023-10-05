0ddddd28449d6b641d27f40b032c5c423053173e
QC20 <xgv866@alumni.ku.dk>
2023-10-05 11:33:42 +0200

    Level-2-upload

---
+++ Game-Level-Designs/Level-2.cpp
@@ -0,0 +1,20 @@
+struct Waypoint {
+  int x;
+  int y;
+};
+
+Waypoint waypoints[] = {
+  {10, 0},   // Start
+  {10, 5},   // First turn
+  {0, 5},   // Second turn
+  {0, 15},   // Third turn
+  {10, 15},   // Fourth turn
+  {10, 10}, // fifth turn
+  {25, 10}, // sixth turn (this is pretty easy)
+  {25, 20}, // seventh turn
+  {17, 20}, // eigth turn
+  {17, 14}, // ninth turn
+  {11, 14}, // tenth turn
+  {11, 27}, // eleventh turn
+  {15, 27}    // End
+};
